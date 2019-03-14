44a1bdfd10aa3d295c2c877a3876562b1563c3c7
alexwh <alexwh@users.noreply.github.com>
2019-03-14 20:02:14 +0000

    add cli parsing

--- alan.py
+++ alan.py
@@ -4,13 +4,14 @@ import logging
 import signal
 import select
 import socket
+import click
 from socketserver import ThreadingTCPServer, StreamRequestHandler
 
 class TCPProxy(StreamRequestHandler):
     def handle(self):
         logging.info("recieved connection at %s", self.connection.getpeername())
         remote = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        remote.connect(("127.0.0.1", 10001))
+        remote.connect((self.server.remote_ip, self.server.remote_port))
         self.exchange(self.connection, remote)
 
     def exchange(self, client, remote):
@@ -32,9 +33,18 @@ class TCPProxy(StreamRequestHandler):
 def handle_signal(sig, frame):
     sys.exit(0)
 
+@click.command()
+@click.option("--local_ip", default="127.0.0.1", help="local ip to listen on")
+@click.option("--local_port", default=10000, help="local port to listen on")
+@click.argument("remote_ip")
+@click.argument("remote_port", type=int)
+def main(remote_ip, remote_port, local_ip, local_port):
+    with ThreadingTCPServer((local_ip, local_port), TCPProxy) as server:
+        server.remote_ip = remote_ip
+        server.remote_port = remote_port
+        server.serve_forever()
+
 
 if __name__ == '__main__':
     signal.signal(signal.SIGINT, handle_signal)
-
-    with ThreadingTCPServer(('127.0.0.1', 10000), TCPProxy) as server:
-        server.serve_forever()
+    main()
