c56b4208fe36bd58f86bbf6f23cfed6a16858982
alexwh <alexwh@users.noreply.github.com>
2019-03-14 20:11:45 +0000

    add logging

--- alan.py
+++ alan.py
@@ -9,8 +9,9 @@ from socketserver import ThreadingTCPServer, StreamRequestHandler
 
 class TCPProxy(StreamRequestHandler):
     def handle(self):
-        logging.info("recieved connection at %s", self.connection.getpeername())
+        logging.info("recieved connection at {}:{}".format(*self.connection.getpeername()))
         remote = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        logging.info(f"connecting to remote: {self.server.remote_ip}:{self.server.remote_port}")
         remote.connect((self.server.remote_ip, self.server.remote_port))
         self.exchange(self.connection, remote)
 
@@ -21,16 +22,19 @@ class TCPProxy(StreamRequestHandler):
 
             if client in readable:
                 data = client.recv(4096)
+                logging.debug(f"reading client data: {data}")
                 if remote.send(data) <= 0:
                     break
 
             if remote in readable:
                 data = remote.recv(4096)
+                logging.debug(f"reading remote data: {data}")
                 if client.send(data) <= 0:
                     break
 
 
 def handle_signal(sig, frame):
+    logging.info("exiting")
     sys.exit(0)
 
 @click.command()
@@ -39,6 +43,9 @@ def handle_signal(sig, frame):
 @click.argument("remote_ip")
 @click.argument("remote_port", type=int)
 def main(remote_ip, remote_port, local_ip, local_port):
+    logging.basicConfig(level=logging.DEBUG)
+
+    logging.info(f"listening on {local_ip}:{local_port}")
     with ThreadingTCPServer((local_ip, local_port), TCPProxy) as server:
         server.remote_ip = remote_ip
         server.remote_port = remote_port
