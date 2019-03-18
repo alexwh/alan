ae3957761e91036af9eed2654aa411a889c8e9f2
alexwh <alexwh@users.noreply.github.com>
2019-03-18 16:58:34 +0000

    qhexedit test

--- alan.py
+++ alan.py
@@ -6,6 +6,8 @@ import select
 import socket
 import click
 from socketserver import ThreadingTCPServer, StreamRequestHandler
+from PyQt5 import QtWidgets
+from qhexedit import QHexEdit
 
 class TCPProxy(StreamRequestHandler):
     def handle(self):
@@ -23,15 +25,27 @@ class TCPProxy(StreamRequestHandler):
             if client in readable:
                 data = client.recv(4096)
                 logging.debug(f"reading client data: {data}")
+                self.server.data += data
                 if remote.send(data) <= 0:
                     break
 
             if remote in readable:
                 data = remote.recv(4096)
                 logging.debug(f"reading remote data: {data}")
+                self.server.data += data
                 if client.send(data) <= 0:
                     break
 
+        if self.server.data:
+            app = QtWidgets.QApplication(sys.argv)
+            mainWin = QHexEdit()
+            mainWin.setData(self.server.data)
+            mainWin.setReadOnly(False)
+            mainWin.resize(600, 400)
+            mainWin.move(300, 300)
+            mainWin.show()
+            app.exec_()
+            self.server.data = bytes()
 
 def handle_signal(sig, frame):
     logging.info("exiting")
@@ -49,6 +63,7 @@ def main(remote_ip, remote_port, local_ip, local_port):
     with ThreadingTCPServer((local_ip, local_port), TCPProxy) as server:
         server.remote_ip = remote_ip
         server.remote_port = remote_port
+        server.data = bytes()
         server.serve_forever()
 
 
