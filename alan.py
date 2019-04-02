587f39a8960537cb54a521b390d5b0670fbf0555
alexwh <alexwh@users.noreply.github.com>
2019-04-02 19:04:09 +0100

    move pyqt stuff to main alan.py file

--- alan.py
+++ alan.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
 import sys
 import logging
-import signal
-import select
 import socket
-import click
-from socketserver import ThreadingTCPServer, StreamRequestHandler
+import select
+import signal
 from PyQt5 import QtWidgets
 from qhexedit import QHexEdit
+from socketserver import ThreadingTCPServer, StreamRequestHandler
+
+import design
 
 class TCPProxy(StreamRequestHandler):
     def handle(self):
@@ -36,36 +37,40 @@ class TCPProxy(StreamRequestHandler):
                 if client.send(data) <= 0:
                     break
 
-        if self.server.data:
-            app = QtWidgets.QApplication(sys.argv)
-            mainWin = QHexEdit()
-            mainWin.setData(self.server.data)
-            mainWin.setReadOnly(False)
-            mainWin.resize(600, 400)
-            mainWin.move(300, 300)
-            mainWin.show()
-            app.exec_()
-            self.server.data = bytes()
+class ExampleApp(QtWidgets.QMainWindow, design.Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.he = QHexEdit()
+        self.pushButton.clicked.connect(self.hexedit)
+
+    def hexedit(self):
+        self.he.show()
+
+    def tcp_handle(self):
+        local_ip = self.listen_ip.text()
+        local_port = self.listen_port.text()
+        remote_ip = self.remote_ip.text()
+        remote_port = self.remote_port.text()
+
+        logging.info(f"listening on {local_ip}:{local_port}")
+        with ThreadingTCPServer((local_ip, local_port), TCPProxy) as server:
+            server.remote_ip = remote_ip
+            server.remote_port = remote_port
+            server.data = bytes()
+            server.handle_request()
+
+
+def main():
+    logging.basicConfig(level=logging.DEBUG)
+    app = QtWidgets.QApplication(sys.argv)
+    form = ExampleApp()
+    form.show()
+    sys.exit(app.exec_())
 
 def handle_signal(sig, frame):
     logging.info("exiting")
     sys.exit(0)
-
-@click.command()
-@click.option("--local_ip", default="127.0.0.1", help="local ip to listen on")
-@click.option("--local_port", default=10000, help="local port to listen on")
-@click.argument("remote_ip")
-@click.argument("remote_port", type=int)
-def main(remote_ip, remote_port, local_ip, local_port):
-    logging.basicConfig(level=logging.DEBUG)
-
-    logging.info(f"listening on {local_ip}:{local_port}")
-    with ThreadingTCPServer((local_ip, local_port), TCPProxy) as server:
-        server.remote_ip = remote_ip
-        server.remote_port = remote_port
-        server.data = bytes()
-        server.serve_forever()
-
 
 if __name__ == '__main__':
     signal.signal(signal.SIGINT, handle_signal)
